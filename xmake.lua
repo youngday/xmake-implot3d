71cfe026388d10941cd96c64f3376fc2c89e7522
youngday <young.day@aliyun.com>
2025-02-22 11:08:08 +0800

    get implot3d 0.2 lib from xrepo

--- xmake.lua
+++ xmake.lua
@@ -6,9 +6,9 @@ add_rules("plugin.compile_commands.autoupdate")
 
 add_requires( "nlohmann_json 3.11.3","simdutf 5.4.15","yaml-cpp 0.8.0", "toml++ 3.4.0","quill 7.5.0","fmt 11.0.2")
 add_requires("matplotplusplus 1.2.1")--gnuplot 2d/3d
-add_requires("imgui 1.91.5", {configs = {glfw= true,opengl3 = true,sdl2 = true}})
+add_requires("imgui 1.91.8", {configs = {glfw= true,opengl3 = true,sdl2 = true}})
 add_requires("glad 0.1.36")
-
+add_requires("implot3d 0.2")
 add_packages( "nlohmann_json","simdutf","yaml-cpp", "toml++","quill","fmt")
 
 add_includedirs("src/utils")
@@ -29,8 +29,6 @@ target("plot")
     set_kind("binary")
     add_files("src/plot/main_implot3d.cpp")
     add_files("src/plot/implot3d_demo.cpp")
-    add_files("src/plot/implot3d/*.cpp")
-    add_includedirs("src/plot/implot3d")
-    add_packages("imgui","glad")
+    add_packages("imgui","glad","implot3d")
 
 
